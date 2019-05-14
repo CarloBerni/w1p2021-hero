f980d0828228f9b9a0fc1b7c74b5a75814d78583
CarloBerni <carlo.berni@hetic.net>
2019-05-14 19:20:57 +0200

    je capte pas un truc

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -1,21 +1,14 @@
 <template>
-  <div class="big__header">
-    <h1 style="margin-bottom: 700px;">{{message}}</h1>
-    <br />
-
+  <div class="homepage">
     <div>
-    <router-link class="button" to="/characters">Votre esclave</router-link>
-    <router-link class="button" to="/characters">Votre esclave</router-link>
+      <router-link class="button" to="/characters">Votre esclave</router-link>
+      <router-link class="button" to="/characters">Votre esclave</router-link>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      message: 'Bonjour'
-    };
-  }
+  
 };
 </script>
