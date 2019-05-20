963670a5192f633a377f001b24e004a317e2546a
CarloBerni <carlo.berni@hetic.net>
2019-05-20 10:09:07 +0200

    recommence

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -1,32 +1,17 @@
 <template>
-  <div class="home">
-    <p>{{data[index].name}}</p>
-    <div class="coucou">
-      <p>{{data[index].text}}</p>
-    </div>
-    <br>
+  <div class="big-header">
+    <h1>{{ message }}</h1>
+    <br />
     <router-link class="button" to="/page">Go to Page</router-link>
-    <br>
   </div>
 </template>
 
-
-
 <script>
-import data from "../data.json";
-
 export default {
-  name: "home",
-
   data() {
     return {
-      data: [],
-      index: 0
+      message: 'Hello Hetic'
     };
-  },
-  created() {
-    this.data = data;
   }
 };
 </script>
- 
--- app/components/Page.vue
+++ app/components/Page.vue
@@ -1,27 +1,13 @@
 <template>
-  <div class="home">
-    <div class="coucou">
-      <p>{{data[index].text}}</p>
-    </div>
-    <br>
+  <div class="big-header">
+    <h1>Page</h1>
+    <br />
     <router-link class="button" to="/">Go to Home</router-link>
   </div>
 </template>
 
 <script>
-import data from "../data.json";
-
 export default {
-  name: "home",
-
-  data() {
-    return {
-      data: [],
-      index: 0
-    };
-  },
-  created() {
-    this.data = data;
-  }
+    
 };
 </script>
